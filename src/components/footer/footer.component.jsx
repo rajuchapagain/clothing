8a12437dc0f4eae0ff60ec949432823c340e28d4
Admin <raju.chapagain@outlook.com>
2020-05-13 19:50:18 +1000

    updated shop data with new data, grouped the cart item with quantity field

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.styles.scss";
+
+const Header = ({ currentUser, hidden }) => (
+  <div className="footer"> Copyright © Amalachour digital works</div>
+);
+
+export default Header;
